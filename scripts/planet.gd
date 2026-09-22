a951ba41bc51317d3f6c5f95d7e1acc6a4b7671f
Steelsphere <uber005@gmail.com>
2026-09-22 00:49:05 -0500

    added temperature simulation kinda

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -1,4 +1,18 @@
 extends MeshInstance3D
 
+var main_temp_buffer = true
+
+@onready var temperature: SubViewport = $Temperature
+@onready var temperature_2: SubViewport = $Temperature2
+
 func _physics_process(delta: float) -> void:
-	rotate_y(delta * 0.1)
+	#rotate_y(delta * 0.1)
+	pass
+	
+func _process(delta: float) -> void:
+	if main_temp_buffer:
+		temperature.render_target_update_mode = SubViewport.UPDATE_ONCE
+		main_temp_buffer = false
+	else:
+		temperature_2.render_target_update_mode = SubViewport.UPDATE_ONCE
+		main_temp_buffer = true
