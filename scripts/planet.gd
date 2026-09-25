451669d3a96c46ea09e5c52837525173928872fd
Steelsphere <uber005@gmail.com>
2026-09-25 00:07:54 -0500

    hmm

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -6,13 +6,16 @@ var main_temp_buffer = true
 @onready var temperature_2: SubViewport = $Temperature2
 
 func _physics_process(delta: float) -> void:
-	#rotate_y(delta * 0.1)
-	pass
+	rotate_y(delta * 0.1)
 	
-func _process(delta: float) -> void:
 	if main_temp_buffer:
 		temperature.render_target_update_mode = SubViewport.UPDATE_ONCE
 		main_temp_buffer = false
 	else:
 		temperature_2.render_target_update_mode = SubViewport.UPDATE_ONCE
 		main_temp_buffer = true
+		
+	var sun_dir = transform.basis * Vector3(0.0, 0.0, 1.0)
+	var temp_map = temperature.get_child(0) as ColorRect
+	var temp_map_mat = temp_map.material as ShaderMaterial
+	temp_map_mat.set_shader_parameter("sun_dir", sun_dir)
